6177f77df81dc0516ec9a637b1337a0504f324d1
Yaman Birla <68591556+cosmic-weber@users.noreply.github.com>
2022-10-28 01:08:20 +0530

    Update main.go

--- slack-test/main.go
+++ slack-test/main.go
@@ -21,8 +21,8 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent){
 
 func main() {
 
-	os.Setenv("SLACK_BOT_TOKEN", "xoxb-4295137471761-4276079093830-S6vP8Yh0Q63aqEFvFYVlv6XX")
-	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A047W2WQDP1-4282573585939-375a7837108a74d1acff871e56d79933710dba69f40e5bcd86176a4a6744c972")
+	os.Setenv("SLACK_BOT_TOKEN", "xoxb-your-bot-token")
+	os.Setenv("SLACK_APP_TOKEN", "xapp-your-app-token")
 
 
 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
